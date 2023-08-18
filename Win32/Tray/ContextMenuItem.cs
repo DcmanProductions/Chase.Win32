87fede891c5c1557b3f19d41deaad8f5edb72093
Drew Chase <dcmanproductions@gmail.com>
2023-08-18 02:28:34 -0400

    renamed context menu to context menu item

---
+++ Win32/Tray/ContextMenuItem.cs
@@ -0,0 +1,12 @@
+﻿/*
+    Chase.Win32 - LFInteractive LLC. 2021-2024
+    Chase.Win32 is a C# library to handle various Win32 API Calls.
+    Licensed under GPL-3.0
+    https://www.gnu.org/licenses/gpl-3.0.en.html#license-text
+*/
+
+namespace Chase.Win32.Tray;
+
+public class ContextMenuItem
+{
+}
